1003db0efd4006679613324be3221444b94fc65c
architecturequeen <olawunmitaylor@gmail.com>
2018-07-26 23:38:09 +0100

    feat: Use indexedDB to cache data

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -2,31 +2,12 @@
  * Common database helper functions.
  */
 class DBHelper {
-
   /**
    * Database URL.
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    return 'http://localhost:1337/restaurants';
-  }
-
-  /**
-   * Fetch all restaurants.
-   */
-  static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const restaurants = JSON.parse(xhr.responseText);
-        callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
-        callback(error, null);
-      }
-    };
-    xhr.send();
+    return "http://localhost:1337/restaurants";
   }
 
   /**
@@ -39,10 +20,12 @@ class DBHelper {
         callback(error, null);
       } else {
         const restaurant = restaurants.find(r => r.id == id);
-        if (restaurant) { // Got the restaurant
+        if (restaurant) {
+          // Got the restaurant
           callback(null, restaurant);
-        } else { // Restaurant does not exist in the database
-          callback('Restaurant does not exist', null);
+        } else {
+          // Restaurant does not exist in the database
+          callback("Restaurant does not exist", null);
         }
       }
     });
@@ -83,17 +66,23 @@ class DBHelper {
   /**
    * Fetch restaurants by a cuisine and a neighborhood with proper error handling.
    */
-  static fetchRestaurantByCuisineAndNeighborhood(cuisine, neighborhood, callback) {
+  static fetchRestaurantByCuisineAndNeighborhood(
+    cuisine,
+    neighborhood,
+    callback
+  ) {
     // Fetch all restaurants
     DBHelper.fetchRestaurants((error, restaurants) => {
       if (error) {
         callback(error, null);
       } else {
-        let results = restaurants
-        if (cuisine != 'all') { // filter by cuisine
+        let results = restaurants;
+        if (cuisine != "all") {
+          // filter by cuisine
           results = results.filter(r => r.cuisine_type == cuisine);
         }
-        if (neighborhood != 'all') { // filter by neighborhood
+        if (neighborhood != "all") {
+          // filter by neighborhood
           results = results.filter(r => r.neighborhood == neighborhood);
         }
         callback(null, results);
@@ -111,9 +100,13 @@ class DBHelper {
         callback(error, null);
       } else {
         // Get all neighborhoods from all restaurants
-        const neighborhoods = restaurants.map((v, i) => restaurants[i].neighborhood)
+        const neighborhoods = restaurants.map(
+          (v, i) => restaurants[i].neighborhood
+        );
         // Remove duplicates from neighborhoods
-        const uniqueNeighborhoods = neighborhoods.filter((v, i) => neighborhoods.indexOf(v) == i)
+        const uniqueNeighborhoods = neighborhoods.filter(
+          (v, i) => neighborhoods.indexOf(v) == i
+        );
         callback(null, uniqueNeighborhoods);
       }
     });
@@ -129,9 +122,11 @@ class DBHelper {
         callback(error, null);
       } else {
         // Get all cuisines from all restaurants
-        const cuisines = restaurants.map((v, i) => restaurants[i].cuisine_type)
+        const cuisines = restaurants.map((v, i) => restaurants[i].cuisine_type);
         // Remove duplicates from cuisines
-        const uniqueCuisines = cuisines.filter((v, i) => cuisines.indexOf(v) == i)
+        const uniqueCuisines = cuisines.filter(
+          (v, i) => cuisines.indexOf(v) == i
+        );
         callback(null, uniqueCuisines);
       }
     });
@@ -141,14 +136,14 @@ class DBHelper {
    * Restaurant page URL.
    */
   static urlForRestaurant(restaurant) {
-    return (`./restaurant.html?id=${restaurant.id}`);
+    return `./restaurant.html?id=${restaurant.id}`;
   }
 
   /**
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`./img/${restaurant.id}.jpg`);
+    return `./img/${restaurant.id}.jpg`;
   }
 
   /**
@@ -160,9 +155,63 @@ class DBHelper {
       title: restaurant.name,
       url: DBHelper.urlForRestaurant(restaurant),
       map: map,
-      animation: google.maps.Animation.DROP}
-    );
+      animation: google.maps.Animation.DROP
+    });
     return marker;
   }
 
+  /**
+   * Fetch all restaurants.
+   */
+  static fetchRestaurants(callback) {
+    var indexedDB =
+      window.indexedDB ||
+      window.mozIndexedDB ||
+      window.webkitIndexedDB ||
+      window.msIndexedDB ||
+      window.shimIndexedDB;
+
+    var open = indexedDB.open("ResturantDatabase", 1);
+
+    open.onupgradeneeded = function() {
+      var db = open.result;
+      var store = db.createObjectStore("RestaurantStore", { keyPath: "id" });
+    };
+
+    open.onsuccess = function() {
+      console.log("db created");
+      var db = open.result;
+      var tx = db.transaction("RestaurantStore", "readwrite");
+      var store = tx.objectStore("RestaurantStore");
+
+      store.get("all").onsuccess = function(res) {
+        // if json result not cached, make a server call
+        if (res.target.result === undefined) {
+          console.log("make a fresh call to server for json data");
+          let xhr = new XMLHttpRequest();
+          xhr.open("GET", DBHelper.DATABASE_URL);
+          xhr.onload = () => {
+            if (xhr.status === 200) {
+              // Got a success response from server!
+              const restaurants = JSON.parse(xhr.responseText);
+              var tx = db.transaction("RestaurantStore", "readwrite");
+              var store = tx.objectStore("RestaurantStore");
+              store.put({ id: "all", restaurants: restaurants });
+              callback(null, restaurants);
+            } else {
+              // Oops!. Got an error from server.
+              const error = `Request failed. Returned status of ${xhr.status}`;
+              callback(error, null);
+            }
+          };
+          xhr.send();
+        }
+        // if json result cached, use cache
+        else {
+          console.log("use cached json data");
+          callback(null, res.target.result.restaurants);
+        }
+      };
+    };
+  }
 }
